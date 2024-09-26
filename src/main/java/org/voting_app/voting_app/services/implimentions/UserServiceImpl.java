e1eeb6c91a0a8bbcf050944477f537f6a9ea20f0
janet <oluwafemijanet85@gmail.com>
2024-09-26 14:01:49 +0100

    janet

--- src/main/java/org/voting_app/voting_app/services/implimentions/UserServiceImpl.java
+++ src/main/java/org/voting_app/voting_app/services/implimentions/UserServiceImpl.java
@@ -2,6 +2,6 @@ package org.voting_app.voting_app.services.implimentions;
 
 public class UserServiceImpl {
     private String dance(String dance){
-        return "janet can " + dance;
+        return "janet can be  " + dance;
     }
 }
